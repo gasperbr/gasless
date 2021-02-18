2910aa985969f45bab80aa1983317308769e6535
Gašper <gasperbr@proxima.si>
2021-02-18 02:13:05 +0100

    trade token logic

--- src/app/components/transaction/swap/swap.component.ts
+++ src/app/components/transaction/swap/swap.component.ts
@@ -11,8 +11,10 @@ export class SwapComponent implements OnInit {
 
   token: TokenInfo;
   tokens: TokenInfo[];
-  tradeAmount: string;
-  tradeAmountValid = false;
+  tradeAmount = "";
+  tradeAmountActual = "";
+  tradeAmountValid = true;
+  tooMuch = false;
   balance: string;
   expectedOutput: string;
 
@@ -27,14 +29,51 @@ export class SwapComponent implements OnInit {
 
   async tokenChange(_token: TokenInfo) {
     this.balance = undefined;
-    this.tradeAmount = undefined;
-    this.balance = await this.ethereumService.getBalanceOfToken(_token.address);
+    this.tradeAmount = "";
+    this.tradeAmountActual = "";
+    this.balance = await this.ethereumService.getBalanceOfToken(_token.address, _token.decimals);
   }
 
   async amountChange(_tradeAmount: string) {
-    _tradeAmount.replace(/[0-9]/g, '')
 
+    this.tradeAmountValid = true;
+    this.tooMuch = false;
+
+    const noNum = _tradeAmount.replace(/[0-9]/g, '')
+    if (noNum !== "" && noNum !== "." || _tradeAmount.length === 0) {
+      this.tradeAmountValid = false;
+      return;
+    }
+    const intAmount = BigInt(this.getPaddedAmount(_tradeAmount, this.token.decimals).replace('.', ''));
+    const intBalance = BigInt(this.balance.replace('.', ''));
+    console.log(this.getPaddedAmount(_tradeAmount, this.token.decimals))
+    console.log(intAmount, intBalance);
+
+    if (intBalance < intAmount) {
+      this.tradeAmountValid = false;
+      this.tooMuch = true;
+    }
+
+    if (!this.tradeAmountValid) {
+      return;
+    }
+
+    this.tradeAmountActual = intAmount.toString(10)
     this.expectedOutput = undefined;
+    // this.expectedOutput = await this.tokenListService.calculateTradeOutput(this.token.address, _tradeAmount.replace('.', ''));
+  }
+
+  getPaddedAmount(amount: string, decimals: number) {
+    if (amount[0] === ".") {
+      amount = "0" + amount;
+    }
+    const dotIndex = amount.indexOf('.')
+    if (dotIndex === -1) {
+      amount = amount + ("0".repeat(decimals));
+    } else {
+      amount = amount + ("0".repeat(decimals - (amount.length - (dotIndex + 1))));
+    }
+    return amount;
   }
 
 }
